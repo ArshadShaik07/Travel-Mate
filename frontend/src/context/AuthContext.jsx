2a5a30b13f9b928e1b84ba0a909cb1b98c1c1466
ArshadShaik07 <arshadshaik2007@gmail.com>
2025-11-25 16:19:42 +0530

    added backend url

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -16,7 +16,7 @@ function AuthProvider({ children }) {
 	const [loading, setLoading] = useState(false);
 	const [updating, setUpdating] = useState(false);
 	axios.defaults.withCredentials = true;
-	const backendUrl = import.meta.env.VITE_BACKEND_URL;
+	const backendUrl = "https://holidaybookingsystem-backend.onrender.com/api";
 
 	async function handleRegister() {
 		try {
